f86325467b3bfe5231b41e4127a314abab6ab287
Герман Леонтьев <gerleonsto@gmail.com>
2023-06-21 00:19:54 +0300

    api weather v.1

--- devman.py
+++ devman.py
@@ -6,9 +6,18 @@ cities = [
     'Череповец',
     'svo'
 ]
+params = {
+    'lang': 'ru',
+}
+url = 'https://wttr.in/?TnMq'
 
-for city in cities:
-    url = f'https://wttr.in/{city}?TnMq&lang=ru'
-    response = requests.get(url=url)
-    response.raise_for_status()
-    print(response.text)
+
+def response(url, cities, params=None):
+    for city in cities:
+        url = f'https://wttr.in/{city}?TnMq'
+        response = requests.get(url=url, params=params)
+        response.raise_for_status()
+        print(response.text)
+
+
+response(url, cities, params)
